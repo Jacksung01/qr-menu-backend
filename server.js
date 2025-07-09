b2572b551ffa60a9bd76f78e62ac61fe4f84e341
Jacksung01 <support@journal.co.th>
2025-07-09 15:37:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
 
 app.use(cors());
 app.use(express.json());
